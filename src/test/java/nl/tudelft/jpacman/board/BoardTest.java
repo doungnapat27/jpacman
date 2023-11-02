fe05fea0c9952870a22e03325a23ee9d95699813
Doungnapat Thiansukont <81945185+doungnapat27@users.noreply.github.com>
2023-11-02 07:18:37 +0000

    fix the method name

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -10,6 +10,9 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class BoardTest {
 
+    /**
+     * set Up 
+     */
     private Board board;
     @BeforeEach
     void setUp() {
@@ -17,33 +20,55 @@ public class BoardTest {
         grid[0][0] = new BasicSquare();
         board = new Board(grid);
     }
+
+    /***
+     * test board not null
+     */
     @Test
-    void TestBoardNotNull(){
+    void testBoardNotNull() {
         assertThat(board).isNotNull();
     }
 
+    /***
+     * test board dimension
+     */
     @Test
-    void TestBoardDimension(){
+    void testBoardDimension() {
         assertThat(board.getHeight()).isEqualTo(1);
         assertThat(board.getWidth()).isEqualTo(1);
     }
+
+    /***
+     * test basic square
+     */
     @Test
-    void TestBasicSquare(){
-        assertThat(board.squareAt(0,0) instanceof BasicSquare).isTrue();
-    }
-    @Test
-    void TestInvariant(){
-        assertThat(board.invariant()).isTrue();
-    }
-    @Test
-    void TestWithinBorders(){
-        assertThat(board.withinBorders(0,0)).isTrue();
+    void testBasicSquare() {
+        assertThat(board.squareAt(0, 0) instanceof BasicSquare).isTrue();
     }
 
+    /***
+     * test invariant
+     */
     @Test
-    void TestSquareAtNull(){
+    void testInvariant() {
+        assertThat(board.invariant()).isTrue();
+    }
+
+    /***
+     * test within borders
+     */
+    @Test
+    void testWithinBorders() {
+        assertThat(board.withinBorders(0, 0)).isTrue();
+    }
+
+    /***
+     * test square
+     */
+    @Test
+    void testSquareAtNull() {
         Square[][] grid = new Square[1][1];
         Board board = new Board(grid);
-        assertThat(board.squareAt(0,0)).isNull();
+        assertThat(board.squareAt(0, 0)).isNull();
     }
 }
