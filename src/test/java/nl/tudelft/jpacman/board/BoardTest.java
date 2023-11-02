3dc6953da94470f562208cb8b7a9e98fc69abf8b
Doungnapat Thiansukont <81945185+doungnapat27@users.noreply.github.com>
2023-11-02 07:15:15 +0000

    Fix the file format

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+/***
+ * This is test cases for board class
+ */
 
 public class BoardTest {
 
